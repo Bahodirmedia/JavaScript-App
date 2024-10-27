bbaabc3f743247d6c289533fd74cdc6428a67ff5
Bahodir <bahodir.media@gmail.com>
2024-10-27 21:07:45 +0500

    Getting data from the server

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,19 +2,35 @@ class ApiService {
 	constructor(baseUrl) {
 		this.url = baseUrl
 	}
-	
+
 	async createPost(post) {
 		try {
 			const request = new Request(this.url + '/posts.json', {
 				method: 'post',
 				body: JSON.stringify(post)
 			})
-			const response = await fetch(request)
-			return await response.json()
+			return useRequest(request)
 		} catch (error) {
 			console.log(error);
 		}
 	}
+
+
+	async fetchPsots() {
+		try {
+			const request = new Request(`${this.url}/posts.json`, {
+				method: 'get'
+			})
+			return useRequest(request)
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+
+async function useRequest(request) {
+	const response = await fetch(request)
+	return await response.json()
 }
 
 export const apiService = new ApiService('https://javascript-app-f814f-default-rtdb.firebaseio.com')
