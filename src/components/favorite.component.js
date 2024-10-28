ad982866140cc256a82752ab711897ec37dcf2c1
Bahodir <bahodir.media@gmail.com>
2024-10-28 23:28:04 +0500

    Displaying the list of favorites

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -1,7 +1,51 @@
 import { Component } from '../core/component';
+import { apiService } from '../services/api.service';
+import { renderPost } from "../templates/post.template";
 
 export class FavoriteComponent extends Component {
-	constructor(id) {
+	constructor(id, options) {
 		super(id)
+
+		this.loader = options.loader
 	}
+
+	init() {
+		this.$el.addEventListener('click', linkClickHandler.bind(this))
+	}
+
+	onShow() {
+		const favorites = JSON.parse(localStorage.getItem('favorites'))
+		const html = renderList(favorites)
+		this.$el.insertAdjacentHTML('afterBegin', html)
+	}
+	
+	onHide() {
+		this.$el.innerHTML = ''
+	}
+}
+
+async function linkClickHandler(event) {
+	event.preventDefault()
+
+	if (event.target.classList.contains('js-link')) {
+		const postId = (event.target.textContent);
+		this.$el.innerHTML = ''
+		this.loader.show()
+		const post = await apiService.fetchpostById(postId)
+		this.loader.hide()
+		this.$el.insertAdjacentHTML('afterBegin', renderPost(post, { withButton: false }))
+	}
+}
+
+
+function renderList(list = []) {
+	if (list.length) {
+		return `
+			<ul>
+				${list.map(i => `<li><a href="#" class="js-link">${i}</a></li>`).join(' ')}
+			</ul>
+		`
+	}
+
+	return `<p class="center">You did not add items yet</p>`
 }
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -26,6 +26,17 @@ class ApiService {
 			console.log(error);
 		}
 	}
+	
+	async fetchpostById(id) {
+		try {
+			const request = new Request(`${this.url}/posts/${id}.json`, {
+				method: 'get'
+			})
+			return useRequest(request)
+		} catch (error) {
+			console.log(error);
+		}
+	}
 }
 
 async function useRequest(request) {
